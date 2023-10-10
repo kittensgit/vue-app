d628633994c2d09f8a07bb8f431ffca58294472e
nika <nikaonime@gmail.com>
2023-10-10 20:30:21 +0300

    add style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <script></script>
 
-<template></template>
+<template>
+    <div class="wrapper">
+        <h1>Weather app</h1>
+        <p>Find out the weather in your city</p>
+    </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.wrapper {
+    width: 900px;
+    height: 500px;
+    border-radius: 50px;
+    background: #1f0f24;
+    padding: 20px;
+    text-align: center;
+    color: white;
+}
+
+.wrapper h1 {
+    margin-top: 20px;
+}
+.wrapper p {
+    margin-top: 20px;
+}
+</style>
